3e9c9ddb9341a4410725f460bfe2c46be34aab06
Dmitrii <dmshulaykin@gmail.com>
2020-05-31 16:25:10 +0300

    added config options for database connections

---
+++ ConfigOptions/MongoOptions.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MoviePicker
+{
+    public class MongoOptions
+    {
+        public string ConnectionString { get; set; }
+
+        public string DatabaseName { get; set; }
+    }
+}
